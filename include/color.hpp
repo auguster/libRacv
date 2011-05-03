efbbde0d62b3ccb5fa49a4a8b5f5724dae03a86e
Rémi Auguste <remi.auguste@lifl.fr>
2011-05-03 17:00:02 +0200

    Ajout du support de color et de matrix

---
+++ include/color.hpp
@@ -0,0 +1,21 @@
+/*
+ * color.hpp
+ *
+ *  Created on: 3 mai 2011
+ *      Author: remi
+ */
+
+#ifndef COLOR_HPP_
+#define COLOR_HPP_
+
+//FIXME make a enum instead of a define ?
+#define RACV_RED_CHANNEL 0
+#define RACV_BLUE_CHANNEL 1
+#define RACV_GREEN_CHANNEL 2
+
+namespace racv {
+void showPixelColor(int color);
+int getChannel(int color, int channel);
+}
+
+#endif /* COLOR_HPP_ */
---
+++ include/matrix.hpp
@@ -0,0 +1,17 @@
+/*
+ * matrix.hpp
+ *
+ *  Created on: 3 mai 2011
+ *      Author: remi
+ */
+
+#ifndef MATRIX_HPP_
+#define MATRIX_HPP_
+
+#include <opencv2/core/core.hpp>
+
+namespace racv {
+	void showMatrix(const cv::Mat matrix);
+}
+
+#endif /* MATRIX_HPP_ */
