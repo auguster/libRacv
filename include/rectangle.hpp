93896acddfa24656811ae787e839722a18dfd7da
Rémi Auguste <remi.auguste@lifl.fr>
2011-09-19 17:09:35 +0200

    Mise à jour de la doc et du fichier rectangle.hpp, normalement ça aurait dû être fait dans le commit précédent ...

--- include/rectangle.hpp
+++ include/rectangle.hpp
@@ -2,6 +2,7 @@
  * rectangle.hpp
  *
  *  Created on: 14 juin 2011
+ *  Last Modification: 19/09/2011
  *      Author: remi
  */
 
@@ -12,12 +13,33 @@
 
 namespace racv {
 
+/**
+ * this function checks if two rectangles are overlapping
+ */
 bool isOverlapping(cv::Rect A, cv::Rect B);
 
 /**
  * Merge vector source with vector destination into destination
  */
 void merge(std::vector<cv::Rect > &source, std::vector<cv::Rect > &destination);
+
+/**
+ * this function transforms the rectangle into a scaled version of itself
+ * even the origins are scaled
+ * this function is for space change
+ */
+void scaleRectangle(cv::Rect &rectangle, double scale);
+
+/**
+ * this function makes sure that a rectangle doesn't go outside the image boundaries
+ */
+void fitRectangle(cv::Mat image, cv::Rect& rectangle);
+
+/**
+ * returns true if bRect overlaps an upscaled (10% by default) version of aRect
+ * NB: if pourcent == 1, just call isOverlapping
+ */
+bool isNear(cv::Rect aRect, cv::Rect bRect, double pourcent = 1.1);
 
 }  // namespace racv
 
