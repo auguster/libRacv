01e8997519625ee6c7e6f3362751ab54fb49c957
Rémi Auguste <remi.auguste@lifl.fr>
2011-10-25 18:09:43 +0900

    Restructuration des includes, ça devient trop le bordel

---
+++ include/libRacv/libRacv.hpp
@@ -0,0 +1,32 @@
+/*
+ *  Created on: 25 mars 2011
+ *  Version: 0.1.1
+ *
+ *  Author: Rémi AUGUSTE
+ *  Email: remi.auguste@lifl.fr
+ */
+
+#ifndef LIBREMICV_HPP_
+#define LIBREMICV_HPP_
+
+#include <colorNormalization/colorNormalization.hpp>
+#include <detection.hpp>
+#include <points.hpp>
+#include <color.hpp>
+#include <matrix.hpp>
+#include <pipes/pipes.hpp>
+#include <person.hpp>
+#include <rectangle.hpp>
+#include <drawing/drawing.hpp>
+#include <video.hpp>
+
+namespace racv
+{
+	/**
+	 * Easy way to test if libRacv is working
+	 */
+	void test();
+} // namespace racv
+
+
+#endif /* LIBREMICV_HPP_ */
